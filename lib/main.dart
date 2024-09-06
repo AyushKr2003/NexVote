29b95ee00979f2148f4b588efa1a52b694baab34
AyushKr <ayushkrsngh2003@gmail.com>
2024-09-07 02:09:51 +0530

    welcome page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nex_vote/nav_screen.dart';
-import 'package:nex_vote/side_bar.dart';
+import 'package:nex_vote/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: NavigationScreen(),
+      home: WelcomePage()
+      // NavigationScreen(),
     );
   }
 }
